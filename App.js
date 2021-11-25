002d0344637b84c3e07766d3cdb4c580dadeb2cc
LucasAcacio <lucas.acacio@liferay.com>
2021-11-25 11:33:42 -0300

    Starting project

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import MainScreen from './Screens/MainScreen';
+import ConfirmationSuccess from './components/ConfirmationSuccess';
+import UserPage from './components/UserPage';
+import WeatherPage from './components/WeatherPage';
+import HomePage from './components/HomePage';
+import RegisterPage from './components/RegisterPage';
+import PlantPage from './components/PlantPage';
+import HarvestPage from './components/HarvestPage';
+import EditPlant from './components/EditPlant';
+import AddPlant from './components/AddPlant';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login"
+        screenOptions={{
+          headerShown: false
+        }}>
+        <Stack.Screen name="Login" component={HomePage}/>
+        <Stack.Screen name="Register" component={RegisterPage}/>
+        <Stack.Screen name="Main" component={MainScreen}/>
+        <Stack.Screen name="UserPage" component={UserPage}/>
+        <Stack.Screen name="WeatherPage" component={WeatherPage}/>
+        <Stack.Screen name="PlantPage" component={PlantPage}/>
+        <Stack.Screen name="HarvestPage" component={HarvestPage}/>
+        <Stack.Screen name="EditPlant" component={EditPlant}/>
+        <Stack.Screen name="AddPlant" component={AddPlant}/>
+        <Stack.Screen name="Confirmation" component={ConfirmationSuccess}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
